487d4ee4216e4b369111fd3ac57959ffb8b159a9
NavinKumar77 <89631106+NavinKumar77@users.noreply.github.com>
2023-06-27 18:24:37 +0530

    Create Operators-Newspaper Agency.c

---
+++ Operators-Newspaper Agency.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    int a,b,c,o=0;
+    scanf("%d",&a);
+    scanf("%d",&b);
+    scanf("%d",&c);
+    o=((a*b)-(a*c))-100;
+    printf("%d",o);
+    return 0;
+}
